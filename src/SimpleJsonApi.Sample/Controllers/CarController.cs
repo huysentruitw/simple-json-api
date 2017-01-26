e9c0d1e100fd5ceeeccf467883f4c03afbc0fca2
Wouter Huysentruit <wouter.huysentruit@cognistreamer.be>
2017-01-26 22:02:15 +0100

    Extend CarController and DriverController with memory backed store

--- src/SimpleJsonApi.Sample/Controllers/CarController.cs
+++ src/SimpleJsonApi.Sample/Controllers/CarController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
 using SimpleJsonApi.Sample.Models;
 
 namespace SimpleJsonApi.Sample.Controllers
@@ -6,10 +9,49 @@ namespace SimpleJsonApi.Sample.Controllers
     [RoutePrefix("api/cars")]
     public class CarController : ApiController
     {
+        private readonly Dictionary<Guid, Car> _cars = new Dictionary<Guid, Car>();
+
+        [HttpGet]
+        [Route("")]
+        [ResponseType(typeof(IEnumerable<Car>))]
+        public IHttpActionResult GetCars()
+        {
+            return Ok(_cars);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ResponseType(typeof(Car))]
+        public IHttpActionResult GetCar(Guid id)
+        {
+            if (!_cars.ContainsKey(id)) return NotFound();
+            return Ok(_cars[id]);
+        }
+
         [HttpPost]
         [Route("")]
         public IHttpActionResult CreateCar(Car car)
         {
+            car.Id = Guid.NewGuid();
+            _cars.Add(car.Id, car);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IHttpActionResult UpdateCar(Guid id, Car car)
+        {
+            if (!_cars.ContainsKey(id)) return NotFound();
+            _cars[id] = car;
+            return Ok();
+        }
+
+        [HttpPatch]
+        [Route("{id}")]
+        public IHttpActionResult PartiallyUpdateCar(Guid id, Changes<Car> carChanges)
+        {
+            if (!_cars.ContainsKey(id)) return NotFound();
+            carChanges.ApplyTo(_cars[id]);
             return Ok();
         }
     }
--- src/SimpleJsonApi.Sample/Controllers/DriverController.cs
+++ src/SimpleJsonApi.Sample/Controllers/DriverController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
+using System.Web.Http.Description;
 using SimpleJsonApi.Sample.Models;
 
 namespace SimpleJsonApi.Sample.Controllers
@@ -7,19 +9,49 @@ namespace SimpleJsonApi.Sample.Controllers
     [RoutePrefix("api/drivers")]
     public class DriverController : ApiController
     {
+        private readonly Dictionary<Guid, Driver> _drivers = new Dictionary<Guid, Driver>();
+
+        [HttpGet]
+        [Route("")]
+        [ResponseType(typeof(IEnumerable<Driver>))]
+        public IHttpActionResult GetDrivers()
+        {
+            return Ok(_drivers);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ResponseType(typeof(Driver))]
+        public IHttpActionResult GetDriver(Guid id)
+        {
+            if (!_drivers.ContainsKey(id)) return NotFound();
+            return Ok(_drivers[id]);
+        }
+
         [HttpPost]
         [Route("")]
         public IHttpActionResult CreateDriver(Driver driver)
         {
+            driver.Id = Guid.NewGuid();
+            _drivers.Add(driver.Id, driver);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IHttpActionResult UpdateDriver(Guid id, Driver driver)
+        {
+            if (!_drivers.ContainsKey(id)) return NotFound();
+            _drivers[id] = driver;
             return Ok();
         }
 
         [HttpPatch]
         [Route("{id}")]
-        public IHttpActionResult UpdateDriver(Guid id, Changes<Driver> driverChanges)
+        public IHttpActionResult PartiallyUpdateDriver(Guid id, Changes<Driver> driverChanges)
         {
-            var driver = new Driver();
-            driverChanges.ApplyTo(driver);
+            if (!_drivers.ContainsKey(id)) return NotFound();
+            driverChanges.ApplyTo(_drivers[id]);
             return Ok();
         }
     }
