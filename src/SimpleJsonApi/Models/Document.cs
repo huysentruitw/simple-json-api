521b5708b01008b0a84a2b759ed7e6d0fc32f7c8
Wouter Huysentruit <wouter.huysentruit@cognistreamer.be>
2017-01-28 20:29:21 +0100

    Add Links property to Document class

--- src/SimpleJsonApi/Models/Document.cs
+++ src/SimpleJsonApi/Models/Document.cs
@@ -6,6 +6,9 @@ namespace SimpleJsonApi.Models
     internal sealed class Document
     {
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public IDictionary<string, string> Links { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public DocumentData Data { get; set; }
 
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
