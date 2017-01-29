b2a741d6a44f56dfc6c6719b9d631c0a49c400b8
Wouter Huysentruit <wouter.huysentruit@cognistreamer.be>
2017-01-29 13:54:05 +0100

    Add object extension methods for parsing dynamic objects to a given type

---
+++ src/SimpleJsonApi/Extensions/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SimpleJsonApi.Extensions
+{
+    internal static class ObjectExtensions
+    {
+        public static object ParseAs(this object @object, Type objectType, JsonSerializer jsonSerializer)
+            => JToken.FromObject(@object, jsonSerializer).ToObject(objectType, jsonSerializer);
+
+        public static TObject ParseAs<TObject>(this object @object, JsonSerializer jsonSerializer)
+            => JToken.FromObject(@object, jsonSerializer).ToObject<TObject>(jsonSerializer);
+    }
+}
