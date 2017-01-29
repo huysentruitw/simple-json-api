586ef8e0ba26e57120db781b466e64a816f2f272
Wouter Huysentruit <wouter.huysentruit@cognistreamer.be>
2017-01-29 13:55:41 +0100

    Generate data and relations in DocumentBuilder

--- src/SimpleJsonApi/DocumentConverters/DocumentBuilder.cs
+++ src/SimpleJsonApi/DocumentConverters/DocumentBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using Newtonsoft.Json;
 using SimpleJsonApi.Configuration;
@@ -30,7 +31,8 @@ namespace SimpleJsonApi.DocumentConverters
 
             var document = new Document
             {
-                Links = GenerateLinks(requestUri)
+                Links = GenerateLinks(requestUri),
+                Data = GenerateData(instance, resourceConfiguration)
             };
 
             return document;
@@ -42,6 +44,60 @@ namespace SimpleJsonApi.DocumentConverters
             {
                 { "Self", requestUri.AbsoluteUri }
             };
+        }
+
+        private DocumentData GenerateData(object instance, IResourceConfiguration configuration)
+        {
+            var mapping = configuration.Mapping;
+            return new DocumentData
+            {
+                Id = mapping.GetId(instance),
+                Type = configuration.TypeName,
+                Attributes = GenerateAttributes(instance, mapping),
+                Relationships = GenerateRelationships(instance, mapping)
+            };
+        }
+
+        private IDictionary<string, object> GenerateAttributes(object instance, IResourceMapping mapping)
+        {
+            var attributeNames = mapping.GetAttributeNames().ToList();
+            if (!attributeNames.Any()) return null;
+
+            var attributes = new Dictionary<string, object>();
+            foreach (var attributeName in attributeNames)
+            {
+                var value = mapping.GetAttributeValue(instance, attributeName);
+                attributes.Add(attributeName, value);
+            }
+
+            return attributes;
+        }
+
+        private IDictionary<string, Relationship> GenerateRelationships(object instance, IResourceMapping mapping)
+        {
+            var relationNames = mapping.GetRelationNames().ToList();
+            if (!relationNames.Any()) return null;
+
+            var relationships = new Dictionary<string, Relationship>();
+            foreach (var relationName in relationNames)
+            {
+                var relationTypeName = _configuration.ResourceConfigurations[mapping.GetResourceTypeOfRelation(relationName)]?.TypeName;
+                if (relationTypeName == null) throw new JsonApiException(CausedBy.Server, $"Resource type for relation {relationName} is not configured");
+
+                var value = mapping.GetRelationValue(instance, relationName);
+                if (value == null) return null;
+
+                if (value is Guid)
+                {
+                    relationships.Add(relationName, new Relationship(relationTypeName, (Guid)value));
+                }
+                else if (value is IEnumerable<Guid>)
+                {
+                    relationships.Add(relationName, new Relationship(relationTypeName, (IEnumerable<Guid>)value));
+                }
+            }
+
+            return relationships;
         }
 
         private static Document SerializeHttpError(HttpError httpError)
