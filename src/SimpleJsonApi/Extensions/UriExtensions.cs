4c9ee4b71f72809de268cff5f7006de3124bdf0a
Wouter Huysentruit <wouter.huysentruit@cognistreamer.be>
2017-01-29 16:26:42 +0100

    Add Uri extension method

---
+++ src/SimpleJsonApi/Extensions/UriExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SimpleJsonApi.Extensions
+{
+    internal static class UriExtensions
+    {
+        public static Uri GetAbsoluteBaseUri(this Uri uri)
+            => new Uri(uri.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped));
+    }
+}
